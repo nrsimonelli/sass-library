a17005f7e76adc35fbe861643584196325640215
nrsimonelli <nrsimonelli@gmail.com>
2021-03-18 23:28:59 -0500

    variables and breakpoints

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './css/style.css';
 function App() {
   return (
     <div className='root'>
-      <div className='nav'>nav</div>
+      <div className='nav'></div>
       <div className='side'>side</div>
       <div className='content'>content</div>
       <div className='content'>more content</div>
